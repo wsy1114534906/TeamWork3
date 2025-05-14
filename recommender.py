8734d61ebdd3311c92cdb6a32cfed73365554650
Erwen Li <90245768+wsy1114534906@users.noreply.github.com>
2025-05-14 19:05:30 +0800

    Update recommender.py
    uniform the data structure

--- recommender.py
+++ recommender.py
@@ -6,10 +6,10 @@ from datetime import datetime
 def rerank_paper(candidate:list[ArxivPaper],corpus:list[dict],model:str='avsolatorio/GIST-small-Embedding-v0') -> list[ArxivPaper]:
     encoder = SentenceTransformer(model)
     #sort corpus by date, from newest to oldest
-    corpus = sorted(corpus,key=lambda x: datetime.strptime(x['data']['dateAdded'], '%Y-%m-%dT%H:%M:%SZ'),reverse=True)
+    corpus = sorted(corpus,key=lambda x: datetime.strptime(x['dateAdded'], '%Y-%m-%dT%H:%M:%SZ'),reverse=True)
     time_decay_weight = 1 / (1 + np.log10(np.arange(len(corpus)) + 1))
     time_decay_weight = time_decay_weight / time_decay_weight.sum()
-    corpus_feature = encoder.encode([paper['data']['abstractNote'] for paper in corpus])
+    corpus_feature = encoder.encode([paper['abstractNote'] for paper in corpus])
     candidate_feature = encoder.encode([paper.summary for paper in candidate])
     sim = encoder.similarity(candidate_feature,corpus_feature) # [n_candidate, n_corpus]
     scores = (sim * time_decay_weight).sum(axis=1) * 10 # [n_candidate]
